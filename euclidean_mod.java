4fa3a89dfafa523303be80fc2667130ed53a6320
Blair Davis <davis.blairrosalyn@gmail.com>
2017-08-24 16:40:40 -0700

    euclidean algorithm, mod implementation

--- euclidean_mod.java
+++ euclidean_mod.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class euclidean_mod {
 	public static void main (String[] args) {
-		//gather input variables from user
+		// Gather input variables from user
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter two numbers to find their gcd.");
 		int a = s.nextInt();
 		int b = s.nextInt();
 		System.out.println("a = " + a + " b = " + b );
 		
-		//Perform Euclidean algorithm to find gcd
+		// Perform Euclidean algorithm to find gcd
 		int gcd = euclidean_algo(a, b);
 
 		// print result
@@ -18,7 +18,7 @@ public class euclidean_mod {
 	}
 
 	public static int euclidean_algo(int a, int b) {
-		//Euclidean algorithm
+		// Euclidean algorithm
 		while( b != 0) {
 			int temporary = b;
 			b = a % b;

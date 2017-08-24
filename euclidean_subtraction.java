3a6a8b16dc7682b0d18a6a46902ae3ab01f39ed0
Blair Davis <davis.blairrosalyn@gmail.com>
2017-08-24 16:41:57 -0700

    euclidean algorithm, subtraction implementation

--- euclidean_subtraction.java
+++ euclidean_subtraction.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class euclidean_subtraction {
 	public static void main (String[] args) {
-		//gather input variables from user
+		// Gather input variables from user
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter two numbers to find their gcd.");
 		int a = s.nextInt();
 		int b = s.nextInt();
 		System.out.println("a = " + a + " b = " + b );
 		
-		//Perform Euclidean algorithm to find gcd
+		// Perform Euclidean algorithm to find gcd
 		int gcd = euclidean_algo(a, b);
 
-		// print result
+		// Print result
 		System.out.println("The gcd of " + a + " and " + b + " is " + gcd);
 	}
 
 	public static int euclidean_algo(int a, int b) {
-		//Euclidean algorithm
+		// Euclidean algorithm
 		while( a != b) {
 			if (a > b) {
 				a = a - b; 

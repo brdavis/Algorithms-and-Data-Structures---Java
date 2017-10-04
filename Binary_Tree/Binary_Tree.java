3cd235d53a6bf3c4fb6a319d2dcf29861ed27307
Blair Davis <davis.blairrosalyn@gmail.com>
2017-10-04 15:49:58 -0700

    wip

--- Binary_Tree/Binary_Tree.java
+++ Binary_Tree/Binary_Tree.java
@@ -1,3 +1,8 @@
+// Binary Search Tree
+// 
+// need to do insert
+
+
 // Node class contains building blocks for binary tree
 class Node {
 	//class variables
@@ -26,13 +31,37 @@ public class Binary_Tree {
 		root = null;
 	}
 
+	//binary tree functions
+
+	//search for value in binary tree
+	public Node search(Node given_node, int value) {
+		//base case of value is in root
+		if (given_node == null || given_node.data == value) {
+			return given_node;
+		} 
+		//value is greater than data in node
+		if (given_node.data > value) {
+			return search(given_node.left, value);
+		}
+
+		//value is less than data in node
+	//	if (given_node.data < value) {
+			return search(given_node.right, value);
+	//	}
+	}
+
 	public static void main(String[] args) {
 		Binary_Tree tree = new Binary_Tree();
 		
 		//create root
 		tree.root = new Node(1);
+
+		//create tree
 		tree.root.left = new Node(2);
 		tree.root.right = new Node(3);
 		tree.root.left.left = new Node(4);
+
+		//search for value
+		
 	}
 }

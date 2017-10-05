3bb52db80f78a0ed63577e8fb58bfa4439ed86d5
Blair Davis <davis.blairrosalyn@gmail.com>
2017-10-05 09:50:37 -0700

    new

--- quick_sort.java
+++ quick_sort.java
@@ -6,6 +6,8 @@ public class quick_sort {
 		// Gather input array from user
 		int[] array = get_array();
 		
+		// Perform quick sort
+		
 		// Print sorted array
 		print_array(array);
 	}
@@ -34,4 +36,37 @@ public class quick_sort {
 		}
 		System.out.println();
 	}
+
+	public static void sort(int[] array, int low, int high) {
+		if(low < high) {
+			int pi = quick(array, low, high);
+			quick(array, low, pi -1);
+			quick(array, pi + 1, high);
+		}
+	}
+
+	public static void quick(int[] array, int low, int high) {
+		// Set pivot
+		int pivot = array[high];
+		
+		int i = low - 1;
+
+		for(int j = low; j <= high-1; j++) {
+			if (array[j] <= pivot) {
+				i++;
+				//swap array[i] array[j]
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		//swap array[i+1] and array[high]
+		int temp = array[i+1];
+		array[i+1] = array[high];
+		array[high] = temp;
+
+		return i + 1;
+		
+	}
+	
 }

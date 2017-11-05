d3718d110df0e1ad7687ad3808b6ec604c1d9bbf
Blair Davis <davis.blairrosalyn@gmail.com>
2017-11-04 20:55:28 -0700

    prints the alphabet

---
+++ For_Fun/print_alphabet.java
@@ -0,0 +1,10 @@
+public class print_alphabet {
+	public static void main(String[] args) {
+		char current_letter;
+	
+		for (current_letter = 'a'; current_letter <= 'z'; current_letter++) {
+			System.out.printf(current_letter + " ");
+		}
+		System.out.println();
+	}
+}

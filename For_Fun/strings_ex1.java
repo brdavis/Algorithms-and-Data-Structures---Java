e40e9d7e30a33d4dbe4101548dc9396e90fd5533
Blair Davis <davis.blairrosalyn@gmail.com>
2017-12-27 22:07:21 -0800

    practice with strings

---
+++ For_Fun/strings_ex1.java
@@ -0,0 +1,9 @@
+public class strings_ex1 {
+	public static void main(String[] args) {
+		String string_1 = "string";
+		String string_2 = "String";
+
+		System.out.println(string_1.compareTo(string_2));
+		System.out.println(string_1.compareToIgnoreCase(string_2));
+	}
+}
